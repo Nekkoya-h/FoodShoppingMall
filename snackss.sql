c622399780522f7b4944fbb360e8b86b221a9a7c
Nekkoya-h <597988953@qq.com>
2020-07-21 11:10:34 +0800

    改了一点小东西

---
+++ snackss.sql
@@ -0,0 +1,19 @@
+INSERT INTO `car` VALUES (1, 3, 12, 1);
+
+INSERT INTO `goods` VALUES (1, '乐事原味薯片（75g)', '原味才是坠吊的！', '薯片.jpg', '6.00');
+INSERT INTO `goods` VALUES (2, 'Calbee/卡乐比水果麦片700g进口早餐即食燕麦片代餐饱腹食品早餐', '水果超多嘎嘣脆，国外进口', '卡乐比.png', '64.9');
+INSERT INTO `goods` VALUES (3, '维他柠檬茶250ML*24盒', '最好喝滴柠檬茶', '柠檬茶.png', '56.9');
+INSERT INTO `goods` VALUES (4, '印尼进口Indomie/营多原味捞面拌面炒面80g*5袋', '好吃是真的好吃，但是一次吃两包会腻，不信你问整个小组', '捞面.png', '12.9');
+INSERT INTO `goods` VALUES (5, '阿宽红油面皮4连包420g', '经典永流传', '红油面皮.png', '15.9');
+INSERT INTO `goods` VALUES (6, '好欢螺螺蛳粉柳州方便速食螺狮粉加辣加臭版400gx6袋', '拉踩小苏专业踩一捧一 好欢螺才是最好吃滴不接受反驳', '螺蛳粉.png', '88');
+INSERT INTO `goods` VALUES (7, '蒙牛真果粒牛奶饮料早餐食品白桃树莓味240g*12包/整箱', '好喝子', '真果粒.png', '68');
+INSERT INTO `goods` VALUES (8, '泰国进口老板仔海苔原味香脆海苔卷27g即食海苔', '其实它叫小老板 好吃子', '小老板.png', '15');
+INSERT INTO `goods` VALUES (9, '巧妈妈 鸡蛋味布甸果冻255G（85G*3杯） 果冻布丁', '俺们泽希酱爱吃布丁但是我懒得找布丁的图了就给个果冻打发一下8', '布丁.png', '12.8');
+INSERT INTO `goods` VALUES (10, '夏威夷果265g休闲小零食小吃零食每日坚果', 'swing心里第一好吃的坚果 第二是只猫', '夏威夷果.png', '25.9');
+INSERT INTO `goods` VALUES (11, '奥利奥白桃乌龙味夹心饼干388g', '一个超市不卖奥利奥说不过去吧', '奥利奥.png', '19.8');
+INSERT INTO `goods` VALUES (12, '百事可乐罐装330ml', '我苏茹茵就是百事宣传大使', '百事可乐.png', '3');
+INSERT INTO `goods` VALUES (13, '娃哈哈 AD钙奶220g', '胡小姐是ad钙奶宣传大使', 'ad钙奶.png', '1.9');
+INSERT INTO `goods` VALUES (14, '数据小姐累了', '给大家看个帅哥吧', '断崖.jpg', '6000');
+
+
+
